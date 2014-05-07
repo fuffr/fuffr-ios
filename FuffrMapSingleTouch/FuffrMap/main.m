3c35f62d5f2357c92687799a1605d278ee80028a
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-05-07 14:19:21 +0200

    Added the original single touch map example as a reference.

---
+++ FuffrMapSingleTouch/FuffrMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FuffrMap
+//
+//  Created by Fuffr on 18/03/14.
+//  Copyright (c) 2014 Fuffr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
---
+++ FuffrMapSingleTouch/FuffrMapTests/FuffrMapTests.m
@@ -0,0 +1,34 @@
+//
+//  FuffrMapTests.m
+//  FuffrMapTests
+//
+//  Created by Fuffr on 18/03/14.
+//  Copyright (c) 2014 Fuffr. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FuffrMapTests : XCTestCase
+
+@end
+
+@implementation FuffrMapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
