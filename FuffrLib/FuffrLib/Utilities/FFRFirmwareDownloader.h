803a39730cbb3c7f01f4d7cf9eb9a7d0235b45fc
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-05-08 15:10:17 +0200

    Removed FuffrOTA (including OTA update in FuffrBox for now).

---
+++ FuffrLib/FuffrLib/Utilities/FFRFirmwareDownloader.h
@@ -0,0 +1,13 @@
+//
+//  FFRFirmwareDownloader.h
+//  FuffrLib
+//
+//  Created by miki on 08/05/14.
+//  Copyright (c) 2014 Fuffr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFRFirmwareDownloader : NSObject
+
+@end
---
+++ FuffrLib/FuffrLib/Utilities/FFRFirmwareDownloader.m
@@ -0,0 +1,13 @@
+//
+//  FFRFirmwareDownloader.m
+//  FuffrLib
+//
+//  Created by miki on 08/05/14.
+//  Copyright (c) 2014 Fuffr. All rights reserved.
+//
+
+#import "FFRFirmwareDownloader.h"
+
+@implementation FFRFirmwareDownloader
+
+@end
