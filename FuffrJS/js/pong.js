3274cdcf23c312f2c99d69d41146b7cffaa1dc0f
Eric Svensson <eric@ericsvensson.se>
2014-05-19 12:12:56 +0430

    FuffrBox: Pong: refactored code for increased readability.

--- FuffrJS/js/pong.js
+++ FuffrJS/js/pong.js
@@ -2,9 +2,8 @@
 var lang = {};
 var game = {};
 var gfx = {};
-var fuffrHandler = {};
-var simulator = {};
 
+/* Called when document is loaded and ready. */
 $(function() {
 	setTimeout(fuffrHandler.checkConnection, fuffrHandler.connTimeoutMs);
 
@@ -16,53 +15,14 @@ $(function() {
 	game.start();
 });
 
-function touchHandler(touches)
-{
-	var foundLeftTouch = false;
-	var foundRightTouch = false;
-
-	for (var i = 0; i < touches.length; ++i)
-	{
-		var touch = touches[i]
-		if (touch.side == fuffr.FFRSideLeft && !foundLeftTouch)
-		{
-			 foundLeftTouch = true;
-			 OnLeftTouch(
-			 	touch.id,
-			 	touch.x,
-			 	touch.y,
-			 	touch.prevx,
-			 	touch.prevy,
-			 	touch.normx,
-			 	touch.normy)
-		}
-		if (touch.side == fuffr.FFRSideRight && !foundRightTouch)
-		{
-			 foundRightTouch = true;
-			 OnRightTouch(
-			 	touch.id,
-			 	touch.x,
-			 	touch.y,
-			 	touch.prevx,
-			 	touch.prevy,
-			 	touch.normx,
-			 	touch.normy)
-		}
-	}
-}
-
-fuffr.on.touchesBegan = touchHandler
-fuffr.on.touchesMoved = touchHandler
-fuffr.on.touchesEnded = touchHandler
-
-lang.missing_init = 'Couldn\'t start the game.';
-lang.fuffr_not_connected = 'Fuffr wasn\'t connected. Do you want to use the simulator?';
-
 game.mainLoopBaseIntervalMs = 21;
 game.levels = 10;
 game.speedIncreasePerLevel = 2;
 game.scorePointsPerLevel = 5;
 game.secondsPerLevel = 30;
+
+lang.missing_init = 'Couldn\'t start the game.';
+lang.fuffr_not_connected = 'Fuffr wasn\'t connected. Do you want to use the simulator?';
 
 game.initialize = function()
 {
@@ -81,94 +41,6 @@ game.initialize = function()
 		score: 0,
 		scoreElement : document.getElementById('score-right'),
 	};
-};
-
-fuffrHandler.wasConnected = false;
-fuffrHandler.connTimeoutMs = 3000;
-
-fuffrHandler.checkConnection = function()
-{
-	if (false === fuffrHandler.wasConnected) {
-		if (confirm(lang.fuffr_not_connected))
-			simulator.enable()
-	}
-};
-
-fuffr.on.connected = function()
-{
-	hyper.log('Fuffr Connected!')
-	fuffr.enableSides(
-		fuffr.FFRSideRight | fuffr.FFRSideLeft,
-		1)
-	fuffrHandler.wasConnected = true;
-}
-
-simulator.enable = function()
-{
-	$('#playfield').addClass('with-simulator');
-	$('#left-touch-area, #right-touch-area').show();
-	resetPlayfield();
-
-	var element = document.getElementById('left-touch-area');
-	Hammer(element).on("touch", function(event) {
-		simulator.handleHammerTouchEvent(event, fuffr.on.touchesBegan);
-	});
-	Hammer(element).on("gesture", function(event) {
-		simulator.handleHammerTouchEvent(event, fuffr.on.touchesMoved);
-	});
-	Hammer(element).on("release", function(event) {
-		simulator.handleHammerTouchEvent(event, fuffr.on.touchesEnded);
-	});
-
-	var element = document.getElementById('right-touch-area');
-	Hammer(element).on("touch", function(event) {
-		simulator.handleHammerTouchEvent(event, fuffr.on.touchesBegan);
-	});
-	Hammer(element).on("gesture", function(event) {
-		simulator.handleHammerTouchEvent(event, fuffr.on.touchesMoved);
-	});
-	Hammer(element).on("release", function(event) {
-		simulator.handleHammerTouchEvent(event, fuffr.on.touchesEnded);
-	});
-};
-
-simulator.disable = function()
-{
-	$('#left-touch-area, #right-touch-area').hide();
-	$('#playfield').removeClass('with-simulator');
-	resetPlayfield();
-};
-
-simulator.handleHammerTouchEvent = function(event, handler)
-{
-	var touches = [];
-
-	for (var t in event.gesture.touches) {
-		var touch = event.gesture.touches[t];
-		if ('object' != typeof touch) continue;
-
-		var side = null;
-		if (touch.target.id == 'left-touch-area')
-			side = fuffr.FFRSideLeft;
-		if (touch.target.id == 'right-touch-area')
-			side = fuffr.FFRSideRight;
-
-		var touchX = touch.x || touch.clientX;
-		var touchY = touch.y || touch.clientY;
-
-		touches.push({
-			id	  : touch.identifier,
-			x	  : touchX,
-			y	  : touchY,
-			prevx : touchX,
-			prevy : touchY,
-			normx : touchX / touch.target.clientWidth,
-			normy : touchY / touch.target.clientHeight,
-			side  : side
-		});
-	}
-
-	handler(touches);
 };
 
 game.start = function()
@@ -527,3 +399,134 @@ function OnLeftTouch(touchId, touchX, touchY, previousX, previousY, normalizedX,
 	var y = (normalizedY * game.playfieldHeight)
 	gfx.paddleLeft.setCenterY(y)
 }
+
+var fuffrHandler = {};
+
+fuffrHandler.wasConnected = false;
+fuffrHandler.connTimeoutMs = 3000;
+
+fuffrHandler.checkConnection = function()
+{
+	if (false === fuffrHandler.wasConnected) {
+		if (confirm(lang.fuffr_not_connected))
+			simulator.enable()
+	}
+};
+
+fuffr.on.connected = function()
+{
+	hyper.log('Fuffr Connected!')
+	fuffr.enableSides(
+		fuffr.FFRSideRight | fuffr.FFRSideLeft,
+		1)
+	fuffrHandler.wasConnected = true;
+}
+
+function touchHandler(touches)
+{
+	var foundLeftTouch = false;
+	var foundRightTouch = false;
+
+	for (var i = 0; i < touches.length; ++i)
+	{
+		var touch = touches[i]
+		if (touch.side == fuffr.FFRSideLeft && !foundLeftTouch)
+		{
+			 foundLeftTouch = true;
+			 OnLeftTouch(
+			 	touch.id,
+			 	touch.x,
+			 	touch.y,
+			 	touch.prevx,
+			 	touch.prevy,
+			 	touch.normx,
+			 	touch.normy)
+		}
+		if (touch.side == fuffr.FFRSideRight && !foundRightTouch)
+		{
+			 foundRightTouch = true;
+			 OnRightTouch(
+			 	touch.id,
+			 	touch.x,
+			 	touch.y,
+			 	touch.prevx,
+			 	touch.prevy,
+			 	touch.normx,
+			 	touch.normy)
+		}
+	}
+}
+
+fuffr.on.touchesBegan = touchHandler
+fuffr.on.touchesMoved = touchHandler
+fuffr.on.touchesEnded = touchHandler
+
+var simulator = {};
+
+simulator.enable = function()
+{
+	$('#playfield').addClass('with-simulator');
+	$('#left-touch-area, #right-touch-area').show();
+	resetPlayfield();
+
+	var element = document.getElementById('left-touch-area');
+	Hammer(element).on("touch", function(event) {
+		simulator.handleHammerTouchEvent(event, fuffr.on.touchesBegan);
+	});
+	Hammer(element).on("gesture", function(event) {
+		simulator.handleHammerTouchEvent(event, fuffr.on.touchesMoved);
+	});
+	Hammer(element).on("release", function(event) {
+		simulator.handleHammerTouchEvent(event, fuffr.on.touchesEnded);
+	});
+
+	var element = document.getElementById('right-touch-area');
+	Hammer(element).on("touch", function(event) {
+		simulator.handleHammerTouchEvent(event, fuffr.on.touchesBegan);
+	});
+	Hammer(element).on("gesture", function(event) {
+		simulator.handleHammerTouchEvent(event, fuffr.on.touchesMoved);
+	});
+	Hammer(element).on("release", function(event) {
+		simulator.handleHammerTouchEvent(event, fuffr.on.touchesEnded);
+	});
+};
+
+simulator.disable = function()
+{
+	$('#left-touch-area, #right-touch-area').hide();
+	$('#playfield').removeClass('with-simulator');
+	resetPlayfield();
+};
+
+simulator.handleHammerTouchEvent = function(event, handler)
+{
+	var touches = [];
+
+	for (var t in event.gesture.touches) {
+		var touch = event.gesture.touches[t];
+		if ('object' != typeof touch) continue;
+
+		var side = null;
+		if (touch.target.id == 'left-touch-area')
+			side = fuffr.FFRSideLeft;
+		if (touch.target.id == 'right-touch-area')
+			side = fuffr.FFRSideRight;
+
+		var touchX = touch.x || touch.clientX;
+		var touchY = touch.y || touch.clientY;
+
+		touches.push({
+			id	  : touch.identifier,
+			x	  : touchX,
+			y	  : touchY,
+			prevx : touchX,
+			prevy : touchY,
+			normx : touchX / touch.target.clientWidth,
+			normy : touchY / touch.target.clientHeight,
+			side  : side
+		});
+	}
+
+	handler(touches);
+};
